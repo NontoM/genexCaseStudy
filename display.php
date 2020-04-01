2a91c0a5c425ac4af67ae7ce5f0e774e2f1b8a6a
root <mbambonontobeko@gmail.com>
2020-04-01 15:31:57 +0200

    genexSurvey

---
+++ display.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main_css_loader.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="jquery/jquery_loader.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <title>Genex Survey</title>
+</head>
+<body>
+<!--header start-->
+<div class="page-header">
+    <div class="header-img">
+        <img src="images/logo1.png" alt="logo" width="140" height="90"  > 
+        </div>
+        </div>
+        <!--header ends-->
+<!--container-->
+<div class="container">
+<p id="main_p">You’re at the end of our survey. Thank you for taking the time to share your feedback.</p>
+</div>
+<!--container Ends-->
+<br><br><br>
+ <!--footer start here-->
+ <footer class="footer">
+      <div class="progress">
+          <div class="progress-bar progress-bar-danger active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+          </div>
+        </div>
+    </footer>
+    <!--footer ends here-->
+    
+</body>
+</html>
